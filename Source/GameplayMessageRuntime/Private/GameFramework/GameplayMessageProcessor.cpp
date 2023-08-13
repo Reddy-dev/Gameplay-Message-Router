f8e509ea97fcaecf3c7bb6bc7b95042a64003fcd
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2023-08-13 15:09:26 -0400

    Init

---
+++ Source/GameplayMessageRuntime/Private/GameFramework/GameplayMessageProcessor.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright Solstice Games
+
+#include "GameFramework/GameplayMessageProcessor.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(GameplayMessageProcessor)
+
+UGameplayMessageProcessor::UGameplayMessageProcessor()
+{
+	
+}
+
+UGameplayMessageProcessor::UGameplayMessageProcessor(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	
+}
+
+void UGameplayMessageProcessor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartListening();
+}
+
+void UGameplayMessageProcessor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	StopListening();
+}
+
+void UGameplayMessageProcessor::StopListening_Implementation()
+{
+	
+}
+
+void UGameplayMessageProcessor::StartListening_Implementation()
+{
+	
+}
---
+++ Source/GameplayMessageRuntime/Public/GameFramework/GameplayMessageProcessor.h
@@ -0,0 +1,26 @@
+﻿// Copyright Solstice Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayMessageProcessor.generated.h"
+
+UCLASS(Abstract, Blueprintable, BlueprintType, ClassGroup=(GameplayMessages), meta=(BlueprintSpawnableComponent))
+class GAMEPLAYMESSAGERUNTIME_API UGameplayMessageProcessor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGameplayMessageProcessor();
+	UGameplayMessageProcessor(const FObjectInitializer& ObjectInitializer);
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Gameplay Messages")
+	void StartListening();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Gameplay Messages")
+	void StopListening();
+};
